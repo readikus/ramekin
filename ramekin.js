21bf83c358345fa1b0041a3afca5c1211e82011c
ian <ianharveyread@gmail.com>
2018-05-30 13:32:55 +0100

    Removed unnecessary comments

--- ramekin.js
+++ ramekin.js
@@ -1,3 +1,14 @@
+/*
+Ramekin server
+
+api/ingest
+api/trends
+
+*/
+
+
+
+
 /**
  * 0.1 requirements:
  *
@@ -11,7 +22,8 @@
  * Implement the @todos.
  * Make it work with bikefeast?
  * gc
- *  
+ * DONE Rank documents for each cluster. The more phrases covered, the higher they rank, if these are equal, rank by the length of the sentence. 
+ *
  * Blog article talking about how it works.
  * Road map in the readme
  * Readme
@@ -22,15 +34,12 @@
  * * 
  * Added stop word removal.
  */
+var cluster        = require('./cluster');
 var extend         = require('extend');
 var moment         = require('moment');
 var natural        = require('natural');
 var NGrams         = natural.NGrams;
-
-// Load the full build. 
-var _ = require('lodash');
-// Load the core build. 
-//var _ = require('lodash/core');
+var _              = require('lodash');
 
 var Ramekin = function( options ) {
 
@@ -48,7 +57,9 @@ var Ramekin = function( options ) {
 
     historyFrequencyTolerance: 1.6,
 
-    similarityThreshold: 0.3
+    similarityThreshold: 0.3,
+
+    trendsTopN: 8
 
   }, options);
 
@@ -64,6 +75,63 @@ var Ramekin = function( options ) {
   natural.PorterStemmer.attach();
 
 };
+
+
+
+
+
+
+
+/**
+ * ingestAll() ingests a set of documents into the current Ramekin.
+ * @param {docs} a set of documents in the format expected format 
+ *   (described above).
+ {_id
+ body
+ subject}
+
+
+ [{"date": JSON date
+@todo: ingest dates in string format.
+@todo: ingest doc.id,doc._id,doc.doc_id all as ID
+@todo: 
+ "body": The text of the document that is to be indexed by the Ramekin.
+ _id": (string) Unique identifier for a document.
+ "subject": (optional) (string) Free-text representation of a specific subject.
+
+
+
+
+
+ */
+Ramekin.prototype.ingestAll = function(docs) {
+  for( var i = 0; i < docs.length; i++ ){
+    this.ingest(docs[i]);
+  }
+};
+
+
+/**
+ * Added option for remove stop words.
+ */
+Ramekin.prototype.ingest = function( doc ) {
+
+  // we may need to revisit what doc data we store
+  this.docs[ doc._id ] = doc;
+  // generate all the [1...n]-grams for the document
+  for( var n = 1; n <= this.options.maxN; n++ ){
+    // create ngrams from the normalised text
+    var ngrams = NGrams.ngrams( this.normalise(doc.body), n );
+    // ingest all the ngrams
+    for( var i = 0; i < ngrams.length; i++ ){
+      this.ingestNGram( ngrams[i], doc );
+    }
+  }
+
+};
+
+
+
 
 
 Ramekin.prototype.ingestNGram = function( ngram, doc ) {
@@ -91,7 +159,7 @@ Ramekin.prototype.ingestNGram = function( ngram, doc ) {
 /**
  * Text analysis stage to take some raw text and convert
  * it into a format that we can ingest optimally.
- * @todo: create a function to covert map the original text
+ * @todo: create a function to map the original text
  * with the normalised version.
  */
 Ramekin.prototype.normalise = function( s ) {
@@ -101,38 +169,6 @@ Ramekin.prototype.normalise = function( s ) {
 
 };
 
-/**
- * Added option for remove stop words.
- */
-Ramekin.prototype.ingest = function( doc ) {
-
-  // we may need to revisit what doc data we store
-  this.docs[ doc.doc_id ] = doc;
-
-  // console.log("Ingesting:", doc);
-  // generate all the [1...n]-grams for the document
-  for( var n = 1; n <= this.options.maxN; n++ ){
-
-    // create ngrams from the normalised text
-    var ngrams = NGrams.ngrams( this.normalise(doc.body), n );
-
-    // ingest all the ngrams
-    for( var i = 0; i < ngrams.length; i++ ){
-      this.ingestNGram( ngrams[i], doc );
-    }
-
-  }
-
-};
-
-Ramekin.prototype.ingestAll = function(docs) {
-
-  for( var i = 0; i < docs.length; i++ ){
-    this.ingest(docs[i]);
-  }
-  //console.log("ngramHistory", JSON.stringify(this.ngramHistory));
-  //process.exit();
-};
 
 Ramekin.prototype.trending = function(options) {
 
@@ -148,17 +184,30 @@ Ramekin.prototype.trending = function(options) {
     options.historyStart = moment(options.historyEnd).subtract(this.options.historyDays, 'day').toDate();
   }
 
-  console.log("trending.options:",options);
+  //console.log("trending.options:",options);
 
   // find all the common phrases used in respective subject, over the past day
   var usedPhrases = this.usedPhrases( options );
   var trendPhrases = [];
+  //var phraseDocs = {}; // duplicated data used later for sorting
+  var docPhrases = {}; // duplicated data used later for sorting
+
+  var setDocPhrases = function(docPhrases, docs, phrases){
+    for(var i =0; i < docs.length; i++){
+      var doc = docs[i];
+      if( !docPhrases.hasOwnProperty(doc) ){
+        docPhrases[ doc ] = [];
+      }
+      docPhrases[ doc ] = docPhrases[ doc ].concat( phrases );
+    }
+  };
 
   // score each phrase from the trend period compared to it's historic use
   for( var i = 0; i < usedPhrases.length; i++ ){
 
-    //   score if the phrase has trended in the last 24 hours
-    var trendRangeCount = this.count( usedPhrases[i], {start: options.start, end: options.end});
+    // score if the phrase has trended in the last 24 hours
+    var trendDocs = this.findDocs( usedPhrases[i], {start: options.start, end: options.end} );
+    var trendRangeCount = trendDocs.length;
     var historyRangeCount = this.count( usedPhrases[i], {start: options.historyStart, end: options.historyEnd});
     var historyDayAverage = historyRangeCount / (this.options.historyDays);
 
@@ -169,169 +218,69 @@ Ramekin.prototype.trending = function(options) {
     if( (trendRangeCount > this.options.minTrendFreq) &&
         (trendRangeCount > historyDayAverage ) ){
 
-      var score = trendRangeCount / (historyDayAverage+1);
+      var score = trendRangeCount / ( historyDayAverage + 1 );
       phrase = {phrase: usedPhrases[i], score: score, 
         historyRangeCount: historyRangeCount,
-        trendRangeCount: trendRangeCount
+        trendRangeCount: trendRangeCount,
+        docs: trendDocs
         };
-
-      /*console.log( JSON.stringify( usedPhrases[i]) + "\t" +
-        trendRangeCount + "\t" +
-        historyRangeCount + "\t" +
-        historyDayAverage );*/
+      //phraseDocs[ usedPhrases[i] ] = trendDocs;
       trendPhrases.push( phrase );
+      setDocPhrases( docPhrases, trendDocs, [phrase.phrase]);
+
     }
 
   }
 
-  console.log( "Trend phrases before removing duplicates:", trendPhrases );
   // remove sub phrases (i.e. "Tour de", compared to "Tour de France")
   trendPhrases = this.removeSubPhrases( trendPhrases );
-  console.log( "Trend phrases after removing duplicates:", trendPhrases );
-
-
-
 
   // rank results
   //@todo: needs making nicer
   trendPhrases.sort(function(a,b){
-/*    if ( a.phrase.historyRangeCount == b.phrase.historyRangeCount ){
-      return b.phrase.length - a.phrase.length;
-    }else{
-      return a.historyRangeCount - b.historyRangeCount;
+    return ( b.score == a.score ) ?
+      b.phrase.length - a.phrase.length :
+      b.score - a.score;
+  });
+
+  // trim to just options.trendsTopN
+  /*if( trendPhrases.length > this.options.trendsTopN ){
+    trendPhrases.splice( this.options.trendsTopN, 
+      trendPhrases.length - this.options.trendsTopN );
+  }*/
+
+  // run the clustering - find the phrase that is most similar to so many others 
+  //  (i.e. i, where sum(i) = max( sum() )
+  var trends = cluster(trendPhrases);
+
+  // rank the documents in each cluster, based on the docs etc.
+  for(var i = 0; i < trends.length; i++){
+    var trend = trends[i];
+    var docs = [];
+    // for each document in that trend, count the number of phrases that match
+    for( var j = 0; j < trend.docs.length; j++){
+      var doc = trend.docs[j];
+      // count the number of phrases from the cluster that are in that doc
+      var matches = _.intersection( docPhrases[ doc ], trend.phrases );
+      docs.push({doc: doc, matches: matches.length});
     }
-*/
-    return b.score - a.score;
-  });
+    // sort...
+    docs.sort(function(a,b){
+      return b.matches - a.matches;
+    });
 
-  console.log("sorted trendPhrases",trendPhrases);
+    // remove unnecessary sort data now it is sorted
+    trend.docs = _.map(docs, 'doc');
+  }
 
-   // remove/cluster overlapping phrases (i.e. "Tour de" and "Tour de France")
-  //  if ( trendPhrases.length > 1 ){
-  var trends = this.simpleCluster(trendPhrases, options);
-  console.log("\r\n\r\n\r\n\r\nSimple Clusterd Phrases:",JSON.stringify(trends));
-  process.exit();
+  // display a nicely formatted summary...
+  for( var i = 0; i < trends.length; i++ ){
 
-//    console.log("trends:",trends);
-//    console.log("after similarity");
-  //  if(){
-//
-  //  }
-
-    // if they are similar, cluster!!
-
-    // maybe compute a similarity matrix at the start??
-
-   // process.exit();
-
- // }
-
-  // match normalised phrases to their
-
-
-  return trends;
-
-};
-
-
-
-/**
- * Calculates the similarity between an array of phrases.
- * It's a bit basic, but similarity = # docs in all phrases / total docs
- */
-/*Ramekin.prototype.similarity = function(phrases, options) {
-
-  console.log("Ramekin.prototype.similarity:", phrases, options);
-
-  var allDocIds = [];
-  var that = this;
-
-  // find *all* docs
-  _.forEach(phrases, function(phrase, key) {
-    console.log("phrase:",phrase);
-
-    allDocIds = allDocIds.concat( that.findDocs( phrase, options ) );
-
-  });
-
-  // typically compares 2 phrases... 
-
-  _.forEach( phrases, function(phrase, key) ) {
-
+    console.log( "Trend " + (i+1) + " : ", this.docs[ trends[i].docs[0] ].body );
 
   }
 
-
-  console.log("After phrases loop");
-
-  process.exit();
-
-  // calculate all the docs
-
-
-
-};*/
-
-/**
- * Calculates the similarity between an array of phrases.
- * It's a bit basic, but similarity = # docs in all phrases / total docs
- */
-Ramekin.prototype.similarity = function(phraseA, phraseB, options) {
-
-  console.log("Ramekin.prototype.similarity:", phraseA, phraseB, options);
-
-  var allDocIds = [];
-  var that = this;
-
-  var docsA = this.findDocs(phraseA, options);
-  var docsB = this.findDocs(phraseB, options);
-  console.log("docsA:",docsA);
-  console.log("docsB:",docsB);
-
-  var matches = _.intersection( docsA, docsB );
-//  var matches = _.intersectionWith(objects, others, _.isEqual);
-
-  var disjunction = (docsA.length - matches.length) +
-    (docsB.length - matches.length);
-
-  var totalDocs = disjunction + (matches.length);
-
-  var similarity = matches.length / Math.min( docsA.length, docsB.length );
-
-  console.log("matches", matches);
-  console.log("totalDocs", totalDocs);
-  console.log("disjunction", disjunction);
-  console.log("calculated similarity:", similarity);
-  console.log("-------------");
-/*
-  Ramekin.prototype.similarity: [ 'chri' ] [ 'chri', 'froom' ] { start: Fri Jul 14 2017 01:00:00 GMT+0100 (BST),
-  end: Sat Jul 15 2017 01:00:00 GMT+0100 (BST),
-  historyEnd: Fri Jul 14 2017 01:00:00 GMT+0100 (BST),
-  historyStart: Tue Jul 04 2017 01:00:00 GMT+0100 (BST) }
-docsA: [ '59690cbdab74ac5a9b92a44f',
-  '596904510718e9290b402af8',
-  '5968edd1a5d36822f9e65e8c',
-  '5968f19052ce36391dc6dcb0',
-  '5968b3ae90de69469ffe0f0c',
-  '596875945413265afa2cf38c',
-  '596853d736823d45237fbb3d' ]
-docsB: [ '59690cbdab74ac5a9b92a44f',
-  '596904510718e9290b402af8',
-  '5968edd1a5d36822f9e65e8c',
-  '5968f19052ce36391dc6dcb0',
-  '5968b3ae90de69469ffe0f0c',
-  '596875945413265afa2cf38c',
-  '596853d736823d45237fbb3d' ]
-matches [ '59690cbdab74ac5a9b92a44f',
-  '596904510718e9290b402af8',
-  '5968edd1a5d36822f9e65e8c',
-  '5968f19052ce36391dc6dcb0',
-  '5968b3ae90de69469ffe0f0c',
-  '596875945413265afa2cf38c',
-  '596853d736823d45237fbb3d' ]
-
-*/
-  return similarity;
+  return trends;
 
 };
 
@@ -350,10 +299,8 @@ Ramekin.prototype.isSubPhrase = function(a, b) {
     a = b;
     b = swap;
   }
-
   // cheat - for now, assume B is the smaller element
   var matchStart = a.indexOf( b[ 0 ] );
-
   // it was found, and check there is space
   if( (matchStart >= 0) && 
     ((matchStart + b.length) <= a.length) ){
@@ -369,7 +316,6 @@ Ramekin.prototype.isSubPhrase = function(a, b) {
   return false;
 
 };
-
 
 /**
  * Improvement: potentially sort results by length before processing.
@@ -391,158 +337,6 @@ Ramekin.prototype.removeSubPhrases = function(trendPhrases) {
   return trendPhrases;
 
 };
-
-
-
-
-Ramekin.prototype.simpleCluster = function( trendPhrases, options ){
-
-  var trends = [];
-
-  for( var i = 0; i < trendPhrases.length; i++ ){
-
-    var trend = {phrases:[ trendPhrases[ i ] ] };
-    trends.push( trend );
-
-    for( var j = i + 1; j < trendPhrases.length; j++ ){
-
-      var similarity = this.similarity( trendPhrases[i].phrase,
-        trendPhrases[j].phrase, options);
-
-      console.log( "measures:",similarity,this.options.similarityThreshold,
-        (similarity >= this.options.similarityThreshold));
-
-      // they appear similar
-      if( similarity >= this.options.similarityThreshold ){
-        console.log("Merging phrases ",trendPhrases[i],trendPhrases[j])         
-        trend.phrases.push( trendPhrases[j] );
-        // remove the element from the array, and update the iterator
-        trendPhrases.splice(j,1);
-        j--;
-      }
-
-    }
-  }
-  return trends;
-
-};
-
-
-Ramekin.prototype.cluster = function( trendPhrases, options ){
-
-/*  var trends = [];
-
-  for( var i = 0; i < trendPhrases.length; i++ ){
-
-    var trend = {phrases:[ trendPhrases[ i ] ] };
-    trends.push( trend );
-
-    for( var j = i + 1; j < trendPhrases.length; j++ ){
-
-      var similarity = this.similarity( trendPhrases[i].phrase,
-        trendPhrases[j].phrase, options);
-
-      console.log( "measures:",similarity,this.options.similarityThreshold,
-        (similarity >= this.options.similarityThreshold));
-
-      // they appear similar
-      if( similarity >= this.options.similarityThreshold ){
-        console.log("Merging phrases ",trendPhrases[i],trendPhrases[j])         
-        trend.phrases.push( trendPhrases[j] );
-        // remove the element from the array, and update the iterator
-        trendPhrases.splice(j,1);
-        j--;
-      }
-
-    }
-  }
-  return trends;*/
-
-  var d = _.fill( Array( trendPhrases.length ), [] );
-
-  // convert everything to a trend phrase
-  //for( var i = 0; i < trendPhrases.length; i++ ){
-
-  // populate the matrix
-  for( var i = 0; i < trendPhrases.length; i++ ){
-    d[i][i] = 0;
-    for( var j = i + 1; j < trendPhrases.length; j++ ){
-      d[i][j] = d[j][i] = this.similarity( trendPhrases[i].phrase,
-        trendPhrases[j].phrase, options);
-    }
-  }
-
-  // find closest match
-  var closetMatch = function(d){   // Only visible inside Restaurant()
-    var maxI = 0, maxJ = 0;
-    // find the closest matches
-    for( var i = 0; i < d.length; i++ ){
-      for( var j = i+1;j<d.length;j++ ){
-        if( d[i][j] > d[maxI][maxJ]){
-          maxI = i, maxJ = j;
-        }
-      }
-    }
-    return {i: maxI, j: maxJ};
-  }
-
-  // perform the closest matches
-  var merges = [];
-
-
-
-  // find the closest matches
-  for( var i = 0; i < trendPhrases.length; i++ ){
-
-    var match = closetMatch(d)
-
-
-  }
-
-/*
-*/
-
-  //
-
- // console.log(d);
-  //process.exit();
-  // find the highest values
-/*
-
-    // cluster the most similar phrases...
-
-
-
-  
-  for( var i = 0; i < trendPhrases.length; i++ ){
-
-    var trend = {phrases:[ trendPhrases[ 0 ] ] };
-    trends.push( trend );
-
-    for( var j = i + 1; j < trendPhrases.length; j++ ){
-
-      var similarity = this.similarity( trendPhrases[i].phrase,
-        trendPhrases[j].phrase, options);
-
-      console.log( "measures:",similarity,this.options.similarityThreshold,
-        (similarity >= this.options.similarityThreshold));
-
-      // they appear similar
-      if( similarity >= this.options.similarityThreshold ){
-        console.log("Merging phrases ",trendPhrases[i],trendPhrases[j])         
-        trend.phrases.push( trendPhrases[j] );
-        // remove the element from the array, and update the iterator
-        trendPhrases.splice(j,1);
-        j--;
-      }
-
-    }
-*/
-
-
-
-};
-
 
 /**
  * Find all the doc ids for a given ngram, matching the options.
@@ -572,24 +366,12 @@ Ramekin.prototype.findDocs = function( ngram, options ){
  */
 Ramekin.prototype.usedPhrases = function(options) {
 
-//  var phrases = {};
-  var phrases = [];//new Set();
-
+  var phrases = [];
   // load all the unique phrases
   for( var n = 1; n <= this.options.maxN; n++ ){
-
     // for each phrase
     for( var i = 0; i < this.ngrams[n].length; i++ ){
-
       var row = this.ngrams[n][i];
-      /*console.log( "options.start: ", options.start );
-      console.log( "ngram.date   : ", row.date );
-      console.log( "ngram.ngram  : ", row.ngram );
-      console.log( "options.end  : ", options.end );
-      console.log( "after start  : ", (row.date >= options.start) );
-      console.log( "befor end    : ", (row.date < options.end) );
-      console.log( "-------------------------------------------------------");*/
-
       // ensure uniqueness and within the date range 
       if( ( _.findWhere(phrases, row.ngram) == null ) &&
        (row.date >= options.start && row.date < options.end) ){
@@ -597,30 +379,14 @@ Ramekin.prototype.usedPhrases = function(options) {
       }
     }
   }
-
   return phrases;
 
 };
 
-/** @todo: update to use the hashmap */
 Ramekin.prototype.count = function(ngram, options) {
 
-  //var count = 0;
-  //var n = ngram.length;
   var matchingDocs = this.findDocs( ngram, options );
   return matchingDocs.length;
-/*)
-  // @todo: refactor using the hash map
-  // for each phrase
-  for( var i = 0; i < this.ngrams[n].length; i++ ){
-    var row = this.ngrams[n][i];
-    if( _.isEqual(ngram, row.ngram) &&
-      row.date >= options.start &&
-      row.date < options.end ){
-      count++;
-    }
-  }  
-  return count;*/
 
 };
 
